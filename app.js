06121b97db2e46cb3c3c441deca1404780f5fdf6
Jahin-Tazwra <tazwarjahin@gmail.com>
2024-11-13 19:51:50 +0600

    Fix prompt

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
   cors({
-    origin: "https://ai-math-notes.netlify.app",
+    origin: process.env.ALLOWED_ORIGIN || "localhost",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
